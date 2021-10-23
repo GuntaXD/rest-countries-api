27c4f423a6bdaf75aea030179f9c188c0be2ba4f
Juan Diaz <juanferreira65@gmail.com>
2021-10-23 14:28:02 -0400

    funcion de solicitud de datos agregada

--- index.js
+++ index.js
@@ -1,39 +1,70 @@
 'use strict'
 
-// const API_URL = "https://restcountries.com/v3/all";
+const API_URL = "https://restcountries.com/v3/all";
 
-// fetch(API_URL)
-// .then( response => response.json() )
-// .then( paises =>{ ordenar(paises);
-// 	for( let i = 0; i < paises.length ; i++ ){
-// 		console.log(paises[i]);
-// 	}
-//  })
-// .catch( error => console.log(error) );
+fetch(API_URL)
+.then( response => response.json() )
+.then( paises =>{ ordenar(paises);
+	mostrar(paises);
+ })
+.catch( error => console.log(error) );
 
-// function mostrar( arreglo ){
-// 	for(let i = 0; i < arreglo.length ; i++ ){
-// 		console.log(arreglo[i].name);
-// 	}
-// }
+function mostrar( arreglo ){
+	arreglo.forEach( pais =>{
+		let card = document.createElement("div");
+		card.classList.add("card");
 
-// function ordenar( array ){
+		let img = document.createElement("img");
+		img.classList.add("card__img");
+		img.setAttribute("src" , `${pais.flags[1]}`);
+
+		let cardName = document.createElement("p");
+		cardName.classList.add("card__name");
+		cardName.appendChild(document.createTextNode(`${pais.name.common}`));
+
+		let cardStats = document.createElement("div");
+		cardStats.classList.add("card__stats");
+
+		let cardPopulation = document.createElement("p");
+		cardPopulation.classList.add("card__population");
+		cardPopulation.appendChild(document.createTextNode(`Population: ${pais.population}`));
+		cardStats.appendChild(cardPopulation);
+
+		let cardRegion = document.createElement("p");
+		cardRegion.classList.add("card__region");
+		cardRegion.appendChild(document.createTextNode(`Region: ${pais.region}`));
+		cardStats.appendChild(cardRegion);
+
+		let cardCapital = document.createElement("p");
+		cardCapital.classList.add("card__capital");
+		cardCapital.appendChild(document.createTextNode(`Capital: ${pais.capital}`));
+		cardStats.appendChild(cardCapital);
+
+		card.appendChild(img);
+		card.appendChild(cardName);
+		card.appendChild(cardStats);
+
+		document.querySelector(".countries").appendChild(card);
+	})
+}
+
+function ordenar( array ){
 	
-// 	for( let i = 0 ; i < array.length; i++ ){
+	for( let i = 0 ; i < array.length; i++ ){
 
-// 		for ( let j = i + 1; j < array.length ; j++ ){
+		for ( let j = i + 1; j < array.length ; j++ ){
 
-// 			if( array[i].name.common > array[ j ].name.common ){
+			if( array[i].name.common > array[ j ].name.common ){
 
-// 				let aux = JSON.parse(JSON.stringify(array[j]));
-// 				array[j] = JSON.parse(JSON.stringify(array[i]));
-// 				array[i] = JSON.parse(JSON.stringify(aux));
+				let aux = JSON.parse(JSON.stringify(array[j]));
+				array[j] = JSON.parse(JSON.stringify(array[i]));
+				array[i] = JSON.parse(JSON.stringify(aux));
 
-// 			}
-// 		}
-// 	}
+			}
+		}
+	}
 
-// }
+}
 
 //funcion del checkbox
 
